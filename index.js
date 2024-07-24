f17ac533981d2a7f5e6b9434611cb05d6a0f7bb6
bossteve1 <bossteve98@gmail.com>
2024-07-24 12:29:02 +0300

    Modify index.js file

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 let progress = document.getElementById("#progress");
 let song = document.getElementById("#song");
 let ctrlIcon = document.getElementById("#ctrlIcon");
+
+song.onloadedmetadata = function(){
+    progress.max = song.duration
+}
